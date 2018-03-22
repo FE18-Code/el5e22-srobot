6eb6e56ce9e0438e0edfb32cf044bc5c6c3d67d1
ams <ams.gh@hmamail.com>
2018-03-22 15:01:36 +0100

    gene_traj : clock + eqns

--- robot.m
+++ robot.m
@@ -22,12 +22,25 @@ m2=5;%kg
 l22=0.5;%m
 k2=100;%NV-1
 L2=1;%m
+I233=2;
 
 % Solid 3
 F3=7.5;%Nm-1S
 m3=5;%kg
 l33=0.5;%m
 k3=10;%NV-1
+I333=0.2;
+
+% time & all
+w=1;
+R=1;
+P=1;
+x0=0;
+y0=0;
+z0=0;
+sim('gene_traj');
+plot3(xf, yf, zf, 'blue');
+grid;
 
 
 % launch simulation
