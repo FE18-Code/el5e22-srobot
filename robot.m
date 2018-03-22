9a53b160f0a99796878da14b3de7f73d81dc6c78
ams <ams.gh@hmamail.com>
2018-03-22 16:57:38 +0100

    MGI v2

--- robot.m
+++ robot.m
@@ -5,11 +5,14 @@ clear all;
 close all;
 clc;
 
-
+l00=0.2;
 % vars
-Kp=3;
-Kd=5;
-
+Kp=2;
+Kd=4;
+Kp2=2;
+Kd2=3;
+Kp3=3;
+Kd3=3;
 % Solid 1
 F1=30;%Nm-1S
 m1=10;%kg
@@ -32,16 +35,21 @@ k3=10;%NV-1
 I333=0.2;
 
 % time & all
-w=1;
-R=1;
-P=1;
-x0=0;
-y0=0;
-z0=0;
-sim('gene_traj');
-plot3(xf, yf, zf, 'blue');
-grid;
-
+w=0.5;
+R=0.5;
+P=0.5;
+x0=1;
+y0=1;
+z0=1;
+%sim('gene_traj');
 
 % launch simulation
 sim('robot_reglage');
+figure(1);
+plot3(xf, yf, zf, 'blue');grid;
+figure(2);
+plot3(xc, yc, zc, 'green');grid;
+figure(3);
+plot3(xf, yf, zf, 'blue');grid;
+hold on
+plot3(xc, yc, zc, 'green');grid;
